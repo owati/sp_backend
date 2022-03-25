48899e6efcc7d9544c26796b760c42306919a45c
owati <mofiyinfoluwaowati@gmail.com>
2022-03-25 07:46:29 +0100

    creating the SKU schema

--- db/sku.js
+++ db/sku.js
@@ -1,5 +1,53 @@
 const mongoose = require("mongoose");
 
 const skuSchema = mongoose.Schema({
-    
+    name : {
+        type : String,
+        required : true
+    },
+    price : {
+        amount : {
+            type : Number
+        },
+        currency : {
+            type : String
+        }
+    },
+
+    quantity : {
+        type : Number,
+        required : true
+    },
+    category : {
+        type :  String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    sizes : {
+        type : [String],
+        required : true
+    },
+    colors : {
+        type : [String],
+        required : true
+    },
+
+    review : {
+        type : Number,
+        default : 0
+    },
+
+    trending : {
+        type : Boolean,
+        default : false
+    }
 })
+
+const Sku = mongoose.model('Sku', skuSchema)
+
+module.exports = {
+    Sku
+}
